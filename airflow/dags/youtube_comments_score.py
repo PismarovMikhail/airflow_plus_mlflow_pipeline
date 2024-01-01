09aa994414492bb0fce053a6cc7a0cd8ffc1cabb
PismarovMikhail <mikepiamarov@gmail.com>
2024-01-01 13:58:51 +0300

    youtube_comments_score.py

---
+++ airflow/dags/youtube_comments_score.py
@@ -0,0 +1,36 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+import datetime as dt
+ 
+args = {
+    "owner": "admin",
+    "start_date": dt.datetime(2023, 12, 31),
+    "retries": 1,
+    "retry_delays": dt.timedelta(minutes=1),
+    "depends_on_past": False
+}
+ 
+with DAG(
+    dag_id='youtube_comments_score',
+    default_args=args,
+    schedule_interval=None,
+    tags=['youtube', 'score'],
+) as dag:
+    get_data = BashOperator(task_id='get_data',
+                            bash_command="python3 /home/mike/xflow/airflow_plus_mlflow_pipeline/scripts/get_data.py",
+                            dag=dag)
+    process_data = BashOperator(task_id='process_data',
+                            bash_command="python3 /home/mike/xflow/airflow_plus_mlflow_pipeline/scriptsprocess_data.py",
+                            dag=dag)
+    train_test_split_data = BashOperator(task_id='train_test_split_data',
+                            bash_command="python3 /home/mike/xflow/airflow_plus_mlflow_pipeline/scripts/train_test_split_data.py",
+                            dag=dag)  
+    train_model = BashOperator(task_id='train_model',
+                            bash_command="python3 /home/mike/xflow/airflow_plus_mlflow_pipeline/scripts/train_model.py",
+                            dag=dag)
+    test_model = BashOperator(task_id='test_model',
+                            bash_command="python3 /home/mike/xflow/airflow_plus_mlflow_pipeline/scripts/test_model.py",
+                            dag=dag)
+    get_data >> process_data >> train_test_split_data >> train_model >> test_model 
