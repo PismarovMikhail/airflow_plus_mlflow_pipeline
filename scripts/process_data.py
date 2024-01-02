1a302e66d5a55ff503620eeb3771c1a8b7ac0266
PismarovMikhail <mikepiamarov@gmail.com>
2024-01-02 03:04:46 +0300

    Add get_data.py and train_model.py for mlflow

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
  
-df = pd.read_csv('/home/mike/xflow/airflow_plus_mlflow_pipeline/datasets/data.csv', header=None)
+df = pd.read_csv('/home/mike/airflow_plus_mlflow_pipeline/datasets/data.csv', header=None)
  
 df[0] = (df[0]-df[0].min())/(df[0].max()-df[0].min())
  
-with open('/home/mike/xflow/airflow_plus_mlflow_pipeline/datasets/data_processed.csv', 'w') as f:
+with open('/home/mike/airflow_plus_mlflow_pipeline/datasets/data_processed.csv', 'w') as f:
     for i, item in enumerate(df[0].values):
         f.write("{},{}\n".format(i, item))
